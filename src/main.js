7333c89235e9237fd69d8725f1fa002b70402d36
Olena Borodkina <elena.kaminnik@gmail.com>
2024-09-18 22:11:00 +0300

    html

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const bodySeach = document.querySelector('body');
+const seachHtml = `
+<div class="container">
+      <form>
+        <input
+          type="text"
+          class="search-input"
+          placeholder="Search images..."
+          name="searchValue"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <ul class="gallery"></ul>
+</div>
+`;
