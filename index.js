bc1b4a75f36912e544b70efd47d1d769ce4035e2
OlenkaOB <elena.kaminnik@gmail.com>
2024-09-18 19:36:34 +0000

    Deploying to gh-pages from @ OlenkaOB/goit-js-hw-11@a99f8f717f5e60044dacf2d210c3ce7f2e7f929b 🚀

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-import"./assets/vendor-Bgfrje-I.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();document.querySelector("body");
+import"./assets/vendor-Bgfrje-I.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=c(e);fetch(e.href,t)}})();const i=document.querySelector("body"),n=`
+<div class="container">
+      <form>
+        <input
+          type="text"
+          class="search-input"
+          placeholder="Search images..."
+          name="searchValue"
+        />
+        <button type="submit">Search</button>
+      </form>
+      <ul class="gallery"></ul>
+</div>
+`;i.insertAdjacentHTML("afterbegin",n);
 //# sourceMappingURL=index.js.map
