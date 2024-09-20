6df621a2916b9a9f20ef8be0144a21098fd88e71
Olena Borodkina <elena.kaminnik@gmail.com>
2024-09-20 16:15:46 +0300

    index1

---
+++ src/js/render-functions.js
@@ -0,0 +1,44 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 200,
+});
+
+export const createGalleryMarkup = photos =>
+  photos
+    .map(
+      photo =>
+        `<li class="galleryEl">
+    <a class="gallery-link" href="${photo.largeImageURL}">
+        <img
+          class="gallery-image"
+          src="${photo.webformatURL}"
+          alt="${photo.tags}"
+          title="${photo.tags}"/>
+      </a>
+        <ul class="data-list">
+          <li class="data-item">
+            <h2 class="data-item-name" >likes</h2>
+            <p class="data-numbers">${photo.likes}</p>
+          </li>
+          <li class="card-text">
+            <h2 class="data-item-name>views</h2>
+            <p class="data-numbers">${photo.views}</p>
+          </li>
+          <li class="card-text">
+            <h2 class="data-item-name>comments</h2>
+            <p class="data-numbers">${photo.comments}</p>
+          </li>
+          <li class="card-text">
+            <h2 class="data-item-name">downloads</h2>
+            <p class="data-numbers">${photo.downloads}</p>
+          </li>
+        </ul>
+      </li>data
+    </ul>`
+    )
+    .join('');
+
+lightbox.refresh();
